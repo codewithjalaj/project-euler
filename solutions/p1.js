e0101a5558206700c62eed019da6abc73779ac41
Jalaj <jalaj@mail.com>
2020-05-14 11:02:22 +0530

    modified the solution

--- solutions/p1.js
+++ solutions/p1.js
@@ -1,6 +1,6 @@
-function multiplesOf3and5(number) {
+function multiplesOf3and5(n) {
 	let num = 0;
-	for (let i = 1; i < number; i++) {
+	for (let i = 1; i < n; i++) {
 		i % 3 === 0 || i % 5 === 0 ? (num += i) : null;
 	}
 	return num;
